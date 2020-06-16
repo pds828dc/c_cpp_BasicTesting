cb6cff7e1d9984a05a0f99ec10b0e09c93bfb55b
rogerYang <a2896522@gmail.com>
2020-06-16 16:02:59 +0800

    add more test patterns

--- cpp_basic_test_bitwise_operator.cpp
+++ cpp_basic_test_bitwise_operator.cpp
@@ -3,6 +3,15 @@
 int main(){
     int flag = 25;
     printf("F>>1 == %d\n",flag>>1);
-    printf("F<<1 == %d\n",flag<<1);
+    printf("F<<1 == %d\n\n",flag<<1);
+    printf("--------------------------test1----------------------------\n\n" );
+
+    int res = (64 >> (2 + 1 - 2)) & (~(1 << 2));
+    printf("res = %d in dec\n", res);
+    printf("res = %x in hex\n", res);
+    printf("--------------------------test2----------------------------\n\n" );
+    unsigned short a = 0xffff;
+    a=~a;
+    printf("%x\n", a);
     return 0;
 }
